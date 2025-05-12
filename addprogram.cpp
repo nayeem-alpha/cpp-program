993d5fa68773e755c0bf6f66df9a4a9d68e15700
Nayeemchowdhury <nayeemchowdhury2002@gmail.com>
2025-05-12 11:13:22 +0530

    first cpp program

---
+++ addprogram.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n) {
+    while(n>9)
+    {
+
+    
+    int sum = 0;
+    while (n != 0) {
+
+        // Extract the last digit
+        int last = n % 10;
+
+        // Add last digit to sum
+        sum += last;
+
+        // Remove the last digit
+        n /= 10;
+    }
+    return sum;
+}
+}
+
+int main() {
+    int n = 12345;
+    cout << sumOfDigits(n);
+    return 0;
+}
